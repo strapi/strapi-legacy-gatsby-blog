aed02c79002286fc340338f29a577feb291a6f0a
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-06-25 23:33:29 +0800

    update gatsby-config.js apiURL API_URL - DEPLOY_URL

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,9 +20,10 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.DEPLOY_URL
-          ? "https://cms-strapi-gatsby.herokuapp.com"
-          : "http://localhost:1337",
+        // apiURL: process.env.DEPLOY_URL
+        // ? "https://cms-strapi-gatsby.herokuapp.com"
+        // : "http://localhost:1337",
+        apiURL: process.env.DEPLOY_URL || "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
