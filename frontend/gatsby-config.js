3a29358bc42327c84307677b40391020547fc9e5
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-06-25 23:19:15 +0800

    update git gatsby-config.js apiURL

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,7 +20,9 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL
+          ? "https://cms-strapi-gatsby.herokuapp.com"
+          : "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
